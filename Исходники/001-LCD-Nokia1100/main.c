c42f7362c68f217824730e86dedff112d2374139
Denis Zheleznyakov <zheleznjakov@mail.ru>
2014-03-01 12:46:14 +0700

    + пример работы с индикатором

--- Исходники/001-LCD-Nokia1100/main.c
+++ Исходники/001-LCD-Nokia1100/main.c
@@ -8,28 +8,41 @@
 
 inline void gpio_init(void)
 {
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOC, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | RCC_AHBPeriph_GPIOC, ENABLE);
 
 	// светодиод
 	PIN_CONFIGURATION(PIN_LED);
 
 	// кнопка
 	PIN_CONFIGURATION(PIN_BUTTON);
+
+	// ЖКИ индикатор
+	PIN_CONFIGURATION(LCD_NOKIA_1100_CS);
+	PIN_CONFIGURATION(LCD_NOKIA_1100_RST);
+	PIN_CONFIGURATION(LCD_NOKIA_1100_SCLK);
+	PIN_CONFIGURATION(LCD_NOKIA_1100_SDA);
 }
 
 void main(void)
 {
 	gpio_init();
 
-	while(1)
+	lcd_nokia1100_init();
+	lcd_nokia1100_set_position(7, 0);
+	lcd_nokia1100_print_string((uint8_t *) "ZiBlog.ru @ 2014");
+	lcd_nokia1100_set_position(0, 0);
+	lcd_nokia1100_print_string((uint8_t *) "STM32F0Discovery");
+	lcd_nokia1100_set_position(4, 1);
+	lcd_nokia1100_print_string((uint8_t *) "LCD Nokia 1100");
+
+	while (1)
 	{
-		if(PIN_SIGNAL(PIN_BUTTON))
-		{
-			PIN_ON(PIN_LED);
-		}
-		else
-		{
-			PIN_OFF(PIN_LED);
-		}
+		PIN_ON(PIN_LED);
+
+		delay_ms(1000);
+
+		PIN_OFF(PIN_LED);
+
+		delay_ms(1000);
 	}
 }
