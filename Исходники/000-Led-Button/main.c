a4b4973d7a9ce0a9b4ffa5d5c645468590969350
Denis Zheleznyakov <zheleznjakov@mail.ru>
2014-03-01 11:30:17 +0700

    + шаблон проекта

---
+++ Исходники/000-Led-Button/main.c
@@ -0,0 +1,9 @@
+/*
+ * File: main.c
+ * Date: 31.08.2012
+ * Denis Zheleznyakov http://ziblog.ru
+ */
+
+void main(void)
+{
+}
---
+++ Исходники/000-Led-Button/main.h
@@ -0,0 +1,16 @@
+/*
+ * File: main.c
+ * Date: 31.08.2012
+ * Denis Zheleznyakov http://ziblog.ru
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "macros.h"
+#include "config.h"
+
+#include "stm32f0xx.h"
+#include "stm32f0xx_rcc.h"
+
+#endif /* MAIN_H_ */
